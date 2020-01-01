6d778d6e7ad63109473323b2b722dae9dcf0f462
weipeng <weipeng>
2020-01-01 18:54:17 +0800

    c++友元类

--- app/src/main/java/com/xiaowei/ndkcpp/SecondActivity.java
+++ app/src/main/java/com/xiaowei/ndkcpp/SecondActivity.java
@@ -7,40 +7,31 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
-
     private NDKCpp ndkCpp;
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         ndkCpp = new NDKCpp();
     }
-
     public void callCppConstrut(View view) {
         ndkCpp.callCppConstruct();
     }
-
     public void callCppNewODelete(View view) {
         ndkCpp.callCppNewOrDelete();
     }
-
     static {
         System.loadLibrary("native-lib");
     }
-
     public void callCppStatic(View view) {
         ndkCpp.callCppStatic();
     }
-
     public void callCppObjectSize(View view) {
         ndkCpp.callCppObjectSize();
     }
-
     public void callCppFriendFuc(View view) {
         ndkCpp.callCppFriendFunc();
     }
-
     public void callCppFriendClass(View view) {
         ndkCpp.callCppFriendClass();
     }
